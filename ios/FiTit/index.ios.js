71cac4b608f366b899d679ac1a12bf1d57580417
Wes Delp <wesdelp@me.com>
2015-09-05 06:29:11 -0400

    two views working

---
+++ ios/FiTit/index.ios.js
@@ -0,0 +1,153 @@
+'use strict';
+
+var React = require('react-native');
+var Button = require('react-native-button');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS,
+  TouchableHighlight
+} = React;
+
+
+var ForNextScene = React.createClass({
+    render() {
+        return (
+            <View style={[styles.navBarContainer, {backgroundColor: '#FFF1E8'}]}>
+                <Text>You came here from the "Next" button!</Text>
+            </View>
+        );
+    }
+});
+
+
+var ForLoadScene = React.createClass({
+  render() {
+    return (
+      <View style={[styles.scene, {backgroundColor: '#ECF6E8'}]}>
+        <Text>WELCOME TO THE LOAD PAGE!</Text>
+      </View>
+    );
+  }
+});
+
+
+var HomeScene = React.createClass({
+
+  render() {
+    return (
+
+      <View style={styles.container}>
+
+        <Text style={styles.welcome}>
+          Welcome to FITit!
+        </Text>
+        <Text style={styles.instructions}>
+          Choose your workout type:
+        </Text>
+
+        <Button style={styles.buttons} onPress={this.onLoadPress}>
+            Load Workout
+        </Button>
+        <Button style={styles.buttons} onPress={this._handleRepPress}>
+            Rep Workout
+        </Button>
+        <Button style={styles.buttons} onPress={this._handleTimedPress}>
+            Timed Workout
+        </Button>
+
+      </View>
+      );
+    },
+
+    onLoadPress() {
+      this.props.navigator.push({
+        title: 'LOAD SCENE',
+        component: ForLoadScene
+      });
+    }
+
+         /*
+          <View style={[styles.scene, {backgroundColor: '#DAF6FF'}]}>
+              <TouchableHighlight onPress={this.onPress}>
+                  <Text>Welcome to the NavigatorIOS Demo. Press here!</Text>
+              </TouchableHighlight>
+          </View>
+          */
+});
+
+var Navigator = React.createClass({
+    onRightButtonPress: function() {
+        this.refs.nav.push({
+            title: 'From Next',
+            component: ForNextScene
+        })
+    },
+
+    render () {
+        return (
+            <NavigatorIOS ref="nav" style={styles.navBarContainer} initialRoute={{
+                component: HomeScene,
+                title: '',
+                rightButtonTitle: 'Next',
+                onRightButtonPress: this.onRightButtonPress
+            }} />
+        )
+    }
+});
+
+
+
+var styles = StyleSheet.create({
+
+  scene: {
+    padding: 10,
+    paddingTop: 74,
+    flex: 1
+    },
+
+    navBarContainer: {
+      flex: 1,
+    },
+
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 30,
+    },
+
+    buttons: {
+      color: 'black',
+      marginBottom: 10,
+    },
+});
+
+/*
+    // Button Handling Functions
+    // TODO send to appropriate view controller
+    _handleLoadPress(event) {
+        console.log('Load Pressed!');
+    },
+    _handleRepPress(event) {
+        console.log('Rep Pressed!');
+    },
+    _handleTimedPress(event) {
+        console.log('Timed Pressed!');
+    }
+*/
+
+AppRegistry.registerComponent('FiTit', () => Navigator);
