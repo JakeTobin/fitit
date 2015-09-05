27af3d6efb6556c48dacca512573703eef99f137
Wes Delp <wesdelp@me.com>
2015-09-05 02:20:02 -0400

    welcome text

--- ios/FiTit/index.ios.js
+++ ios/FiTit/index.ios.js
@@ -17,14 +17,10 @@ var FiTit = React.createClass({
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to FITit!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Choose your workout type:
         </Text>
       </View>
     );
@@ -36,7 +32,7 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#E6E6E6',
   },
   welcome: {
     fontSize: 20,
