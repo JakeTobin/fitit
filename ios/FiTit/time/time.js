050e05f7bc857e8fbe4a0bde4363ef9236a45656
Wes Delp <wesdelp@me.com>
2015-09-05 13:50:25 -0400

    created time scene skeleton

--- ios/FiTit/time/time.js
+++ ios/FiTit/time/time.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react-native');
+var Button = require('react-native-button');
 
 var {
     StyleSheet,
@@ -8,26 +9,62 @@ var {
     Component,
     Text,
     View,
+    TextInput,
     TouchableHighlight
     } = React;
 
+var TimeScene = React.createClass({
+  render() {
+      return (
+          <View style={[styles.container, {backgroundColor: '#ECF6E8'}]}>
+              <Text style={{marginBottom:20}}>Squats</Text>
+              <TextInput
+                style={[styles.textBoxes]}
+                value="Previous Time"
+                editable='false'
+                textAlign='center'
+                /*
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}*/
+              />
+              <TextInput
+                style={[styles.textBoxes]}
+                placeholder="Current Time"
+                keyboardType='numeric'
+                textAlign='center'
+                /*
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}*/
+              />
+              <Button style={styles.buttons} onPress={this.onStartPress}>
+                  Start
+              </Button>
+              <Button style={styles.buttons} onPress={this.onClearPress}>
+                  Clear
+              </Button>
+              <Button style={styles.buttons} onPress={this.onDonePress}>
+                  Done
+              </Button>
+          </View>
+      );
+  }
+});
+
 var styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        paddingTop: 75,
+        alignItems: 'center'
+    },
+
+    textBoxes: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      alignSelf:'center',
+      width: 200,
+      marginBottom: 20
     }
 });
-
-var TimeScene = React.createClass({
-    render() {
-        return (
-
-            <View style={[styles.scene, {backgroundColor: '#ECF6E8'}]}>
-                <Text>WELCOME TO THE Time PAGE!</Text>
-            </View>
-        );
-    }
-});
-
-
 
 module.exports = TimeScene;
