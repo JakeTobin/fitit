4bd48e383395ace3305a7cd86466f4f58195747e
JeffTobin <jeff@tobin.io>
2015-09-05 23:05:25 -0400

    cleaned up package.json

--- web/fitit/client/main.jsx
+++ web/fitit/client/main.jsx
@@ -1,18 +1,3 @@
-require('./assets/app.css');
-
-
 var React = require('react');
-var Router = require('react-router');
-var routes = require('./config/routes.jsx');
-let injectTapEventPlugin = require("react-tap-event-plugin");
-let mui = require('material-ui');
-let RaisedButton = mui.RaisedButton;
-let Dialog = mui.Dialog;
-let ThemeManager = new mui.Styles.ThemeManager();
-let Colors = mui.Styles.Colors;
-
-injectTapEventPlugin();
-
-Router.run(routes, Router.HistoryLocation, function(Handler) {
-  React.render(<Handler/>, document.body);
-});
+var menu = require('./components/menu.jsx');
+var list = require('./components/list.jsx');
